1a2de349b01c502816f33e56432d72da18b9235e
kcherechecha <115093902+kcherechecha@users.noreply.github.com>
2023-09-17 17:38:53 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_WORD_LENGTH 30
+
+int main () {
+    FILE *fp;
+    char word[MAX_WORD_LENGTH + 1];
+    char vowels[] = "aeiou";
+    char filePath[200];
+
+    printf("Введіть путь до файлу: ");
+    scanf("%s", filePath);
+
+    fp = fopen(filePath, "r");
+    if (fp == NULL) {
+        printf("Не вдалося відкрити файл");
+        return 1;
+    }
+
+    printf("Слова, які складаються лише з голосних літер:\n");
+    while (fscanf(fp, "%s", word) != EOF) {
+        int vowelOnlyWord = 1;
+        for (int i = 0; i < strlen(word); i++) {
+            if (!isalpha(word[i]) || !strchr(vowels, word[i])) {
+                vowelOnlyWord = 0;
+                break;
+            }
+        }
+        if (vowelOnlyWord) {
+            printf("%s\n", word);
+        }
+    }
+
+    fclose(fp);
+    return 0;
+}
